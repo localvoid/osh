f48a296452cc6bd6b838c4764d96f5b574b21f92
Boris Kaul <localvoid@gmail.com>
2017-10-27 13:57:46 +0700

    Add reserved keywords to go preset

--- packages/osh-code-go/src/preset.ts
+++ packages/osh-code-go/src/preset.ts
@@ -1,26 +1,60 @@
-import { TChildren, ContextNode, ComponentNode, Context, context, component } from "osh";
-import { INDENT_LEVEL, INDENT_STRING, COMMENT_CONFIG } from "osh-code";
+import { TChildren, TNode, ComponentNode, Context, context, component } from "osh";
+import { INDENT_LEVEL, INDENT_STRING, COMMENT_CONFIG, scope, declSymbol } from "osh-code";
 
-export function GOCode(ctx: Context, children: TChildren[]): ContextNode {
+const RESERVED_KEYWORDS = Symbol("ReservedKeywords");
+
+const RESERVED_KEYWORDS_SYMBOLS = [
+  "break",
+  "default",
+  "func",
+  "interface",
+  "select",
+  "case",
+  "defer",
+  "go",
+  "map",
+  "struct",
+  "chan",
+  "else",
+  "goto",
+  "package",
+  "switch",
+  "const",
+  "fallthrough",
+  "if",
+  "range",
+  "type",
+  "continue",
+  "for",
+  "import",
+  "return",
+  "var",
+].map((s) => declSymbol(s, s));
+
+export function GOCode(ctx: Context, children: TChildren[]): TNode {
   const indentLevel = ctx[INDENT_LEVEL];
-  return context(
-    {
-      [INDENT_LEVEL]: indentLevel === void 0 ? 0 : indentLevel,
-      [INDENT_STRING]: "\t",
-      [COMMENT_CONFIG]: {
-        inlineStart: "/*",
-        inlineEnd: "*/",
-        blockStart: "/*",
-        blockEnd: "*/",
-        blockContinue: null,
-        docStart: null,
-        docEnd: null,
-        docContinue: "//",
-        start: "//",
+  return scope({
+    type: RESERVED_KEYWORDS,
+    symbols: RESERVED_KEYWORDS_SYMBOLS,
+    children: context(
+      {
+        [INDENT_LEVEL]: indentLevel === void 0 ? 0 : indentLevel,
+        [INDENT_STRING]: "\t",
+        [COMMENT_CONFIG]: {
+          inlineStart: "/*",
+          inlineEnd: "*/",
+          blockStart: "/*",
+          blockEnd: "*/",
+          blockContinue: null,
+          docStart: null,
+          docEnd: null,
+          docContinue: "//",
+          start: "//",
+        },
       },
-    },
-    ...children,
-  );
+      children,
+    ),
+  });
 }
 
 export function goCode(...children: TChildren[]): ComponentNode<TChildren[]> {

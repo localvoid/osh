ac2cb983c0fbf4d7ebb7b31ca977a0cc4a06fdf9
Boris Kaul <localvoid@gmail.com>
2017-11-08 13:37:08 +0700

    Add comments and tests for zone function

--- packages/osh/__tests__/index.ts
+++ packages/osh/__tests__/index.ts
@@ -1,2 +1,3 @@
 import "./tnode";
 import "./render";
+import "./zone";
--- packages/osh/src/zone.ts
+++ packages/osh/src/zone.ts
@@ -1,11 +1,21 @@
 import { Context } from "./context";
-import { TNode, TChildren, component } from "./tnode";
+import { TChildren, ComponentNode, component } from "./tnode";
 
-interface WithDisplayName {
+export interface WithDisplayName {
   displayName?: string;
 }
 
-export function zone(name: string, ...children: TChildren[]): TNode {
+/**
+ * zone generates a component with a specified name and instantiates a component node for this component.
+ *
+ * Zones are useful for debugging purposes. When exception is thrown, components stack traces will include zone
+ * components.
+ *
+ * @param name Zone name.
+ * @param children Children nodes.
+ * @returns Zone component node.
+ */
+export function zone(name: string, ...children: TChildren[]): ComponentNode<TChildren[]> {
   const fn: ((ctx: Context, c: TChildren[]) => TChildren) & WithDisplayName = (ctx: Context, c: TChildren[]) => c;
   fn.displayName = `Zone: ${name}`;
   return component(fn, children);
